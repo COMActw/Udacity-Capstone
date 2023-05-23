058f3ec5b43f251d8e40255cf0491fd363d40690
Coma <ndhhung1011@gmail.com>
2023-05-23 09:57:44 +0700

    add shell

--- .circleci/files/deployment.sh
+++ .circleci/files/deployment.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
 cat << EOF > ./deployment.yml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+    name: udacity-capstone
+spec:
+    replicas: 1
+    selector:
+        matchLabels:
+            app: udacity-capstone
+    template:
+        metadata:
+            labels:
+                app: udacity-capstone
+        spec:
+            containers:
+            - name: udacity-capstone
+              image: $1
+              ports:
+              - containerPort: 3000
 
+---
+
+apiVersion: v1
+kind: Service
+metadata:
+    name: udacitycapstone-svc
+spec:
+    type: NodePort
+    ports:
+    - port: 3000
+      targetPort: 3000
+      nodePort: 30080
+    selector:
+        app: udacity-capstone
 EOF
